f74b9f105a8f08780eecf16abd0f2df9b946fbce
Sławomir Jopek <pwnsoon@gmail.com>
2017-10-07 21:18:45 +0200

    update

--- app/server.js
+++ app/server.js
@@ -1,1 +1,12 @@
-console.log("server file here :)");
+import express from "express";
+
+const app = new express();
+
+app.get("/", function (req, res) {
+    res.send("Server running :)")
+});
+
+const port = process.env.PORT || 1111;
+app.listen(port, () => {
+    console.log(`API server running on ${port} port.`);
+});
