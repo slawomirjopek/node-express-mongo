946d473c45a0c9d6ccfb2343f0c6b23ef02a8e8c
Sławomir Jopek <pwnsoon@gmail.com>
2017-10-08 14:04:47 +0200

    update added api get, delete

--- app/server.js
+++ app/server.js
@@ -1,22 +1,46 @@
 import express from "express";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import CONFIG from "./config/config";
 import cars from "./models/cars";
 
 mongoose.connect(`mongodb://${CONFIG.db.host}:${CONFIG.db.port}/${CONFIG.db.name}`);
 
 const app = new express();
+app.use(bodyParser.json());
 
-app.post("/cars", function (req, res) {
+app.post("/cars", (req, res) => {
     const car = req.body;
 
-    cars.create(car, (err, cars) => {
+    cars.create(car, (err, data) => {
         if (err) {
             throw err;
         }
-        res.json(cars);
+        res.json(data);
     })
 });
+
+app.get("/cars", (req, res) => {
+    cars.find((err, data) => {
+        if (err) {
+            throw err;
+        }
+        res.json(data);
+    });
+});
+
+app.delete("/cars/:id", (req, res) => {
+    const query = {_id: req.params.id};
+
+    cars.remove(query, (err, data) => {
+        if (err) {
+            throw err;
+        }
+        res.json(data);
+    });
+});
+
+// @TODO app.put
 
 const port = process.env.PORT || CONFIG.express.port;
 app.listen(port, () => {
